a2e3fdceb34d55e7fb07f9a2b623c3699a3aa680
Carlos Solsona <carlos.solsona@gmail.com>
2014-02-18 00:33:54 +0100

    Add jQuery UI. Add Foundation. Create base html and base visualization. Add todo template

---
+++ js/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+require.config({
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: [
+                'underscore',
+                'jquery'
+            ],
+            exports: 'Backbone'
+        },
+        jqueryui: {
+            deps: ['jquery'],
+            exports: '$'
+        },
+        backboneLocalstorage: {
+            deps: ['backbone'],
+            exports: 'Store'
+        }
+    },
+    paths: {
+        jquery: '../vendors/jquery',
+        jqueryui: '../vendors/jquery.ui',
+        underscore: '../vendors/underscore',
+        backbone: '../vendors/backbone',
+        backboneLocalstorage: '../vendors/backbone.localStorage',
+        text: '../vendors/text'
+    }
+});
+
+require([
+    'jqueryui',
+    'backbone'
+], function ($, Backbone) {
+    Backbone.history.start();
+    $( "#datepicker" ).datepicker();
+    $( "#datepicker" ).datepicker("option", "dateFormat", "dd/mm/yy");
+});
