63b4cf6b01c9867d8ed3a9ecf6a95a365a97cd80
Carlos Solsona <carlos.solsona@gmail.com>
2014-02-20 00:09:18 +0100

    Add item view

---
+++ js/views/todo.js
@@ -0,0 +1,28 @@
+/*global define*/
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'text!templates/todo-item.html',
+    'common/common',
+    'common/todo_state'
+], function ($, _, Backbone, todoTemplate, Common, TSTATE) {
+    'use strict';
+
+    var TodoView = Backbone.View.extend({
+
+        tagName:  'li',
+
+        template: _.template(todoTemplate),
+
+        initialize: function() {
+            this.listenTo(this.model, 'change', this.render);
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            this.$input = this.$('.edit-header');
+            return this;
+        },
+
+});
